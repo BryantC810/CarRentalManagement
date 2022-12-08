1b68d9ac27839353974208a8b2b26d420a9ce26f
CHIA SHAN YANG, BRYANT 2100554A <2100554A@student.tp.edu.sg>
2022-12-08 17:46:33 +0800

    Added database and default data Added Blazor List Pages with Delete function

---
+++ CarRentalManagement/Server/Data/Migrations/20221130024940_fixedseedfile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarRentalManagement.Server.Data.Migrations
+{
+    public partial class fixedseedfile : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Colours",
+                columns: new[] { "Id", "CreatedBy", "DateCreated", "DateUpdated", "Name", "UpdatedBy" },
+                values: new object[] { 1, "System", new DateTime(2022, 11, 30, 10, 49, 39, 527, DateTimeKind.Local).AddTicks(1884), new DateTime(2022, 11, 30, 10, 49, 39, 529, DateTimeKind.Local).AddTicks(5673), "Black", "System" });
+
+            migrationBuilder.InsertData(
+                table: "Colours",
+                columns: new[] { "Id", "CreatedBy", "DateCreated", "DateUpdated", "Name", "UpdatedBy" },
+                values: new object[] { 2, "System", new DateTime(2022, 11, 30, 10, 49, 39, 529, DateTimeKind.Local).AddTicks(6864), new DateTime(2022, 11, 30, 10, 49, 39, 529, DateTimeKind.Local).AddTicks(6872), "Blue", "System" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Colours",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Colours",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
---
+++ CarRentalManagement/Server/Data/Migrations/20221130025147_addmakeseedconfigindbcontext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarRentalManagement.Server.Data.Migrations
+{
+    public partial class addmakeseedconfigindbcontext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Colours",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2022, 11, 30, 10, 51, 47, 278, DateTimeKind.Local).AddTicks(4569), new DateTime(2022, 11, 30, 10, 51, 47, 279, DateTimeKind.Local).AddTicks(7558) });
+
+            migrationBuilder.UpdateData(
+                table: "Colours",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2022, 11, 30, 10, 51, 47, 280, DateTimeKind.Local).AddTicks(1460), new DateTime(2022, 11, 30, 10, 51, 47, 280, DateTimeKind.Local).AddTicks(1474) });
+
+            migrationBuilder.InsertData(
+                table: "Makes",
+                columns: new[] { "Id", "CreatedBy", "DateCreated", "DateUpdated", "Name", "UpdatedBy" },
+                values: new object[,]
+                {
+                    { 1, "System", new DateTime(2022, 11, 30, 10, 51, 47, 281, DateTimeKind.Local).AddTicks(6520), new DateTime(2022, 11, 30, 10, 51, 47, 281, DateTimeKind.Local).AddTicks(6543), "BMW", "System" },
+                    { 2, "System", new DateTime(2022, 11, 30, 10, 51, 47, 281, DateTimeKind.Local).AddTicks(6547), new DateTime(2022, 11, 30, 10, 51, 47, 281, DateTimeKind.Local).AddTicks(6548), "Toyota", "System" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Makes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Makes",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Colours",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2022, 11, 30, 10, 49, 39, 527, DateTimeKind.Local).AddTicks(1884), new DateTime(2022, 11, 30, 10, 49, 39, 529, DateTimeKind.Local).AddTicks(5673) });
+
+            migrationBuilder.UpdateData(
+                table: "Colours",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2022, 11, 30, 10, 49, 39, 529, DateTimeKind.Local).AddTicks(6864), new DateTime(2022, 11, 30, 10, 49, 39, 529, DateTimeKind.Local).AddTicks(6872) });
+        }
+    }
+}
